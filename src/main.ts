f138ab6cd115bffcb218b162225b0722384cf546
Tilkeii <imtilkeii@gmail.com>
2019-03-21 18:22:42 +0100

    add Vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vuetify from "vuetify/lib";
+import "vuetify/src/stylus/app.styl";
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify, {
+    iconfont: "md",
+});
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <v-layout>
+        <HelloWorld :title="msg"/>
+    </v-layout>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
-  },
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+    msg: string = "Welcome to Your Vue.js + TypeScript + Vuetify App";
+
+    onClick(): void {
+        alert("Bonjour");
+    }
+}
+
 </script>
