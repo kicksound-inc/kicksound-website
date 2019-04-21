34437aa85f0fe5f5c22a0e5b084c088fb13dd1c6
Tilkeii <imtilkeii@gmail.com>
2019-04-21 23:19:46 +0200

    cleanup router

--- src/router.ts
+++ src/router.ts
@@ -16,27 +16,18 @@ export default new Router({
         {
             path: "/login",
             name: "Login",
-            component: () => import(/* webpackChunkName: "about" */ "./views/Login.vue"),
+            component: () => import("./views/Login.vue"),
         },
         {
             path: "/register",
             name: "Register",
-            component: () => import(/* webpackChunkName: "about" */ "./views/Register.vue"),
+            component: () => import("./views/Register.vue"),
         },
         {
             path: "/dashboard",
             name: "Dashboard",
-            component: () => import(/* webpackChunkName: "about" */ "./views/Dashboard.vue"),
+            component: () => import("./views/Dashboard.vue"),
 
         }
-        //{
-        //path: "/about",
-        //name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        //component: () => import(/* webpackChunkName: "about" */ "./views/About.vue"),
-        //},
-
     ],
 });
