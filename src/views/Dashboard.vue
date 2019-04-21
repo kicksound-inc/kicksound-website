fa3341eecf077d4f588c038f334a132d04140f30
Tilkeii <imtilkeii@gmail.com>
2019-04-21 23:03:38 +0200

    add parallax

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container>
+        <h1>Dashboard</h1>
+    </v-container>
+</template>
+
+<style scoped>
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class Dashboard extends Vue {}
+</script>
