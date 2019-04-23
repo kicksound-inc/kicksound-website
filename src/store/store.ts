3303f85358944059ee9f2af0087dc8629f08f7cc
Florian <florian@gouarin.info>
2019-04-23 19:58:27 +0200

    config & inistialized vuex-persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,8 +3,13 @@ import Vuex, {StoreOptions} from "vuex";
 import Drawer from "@/store/modules/drawer";
 import User from "@/store/modules/user";
 import {IRootState} from "@/store/types";
+import VuexPersistence from 'vuex-persist';
 
 Vue.use(Vuex);
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 const store: StoreOptions<IRootState> = {
     state: {
@@ -13,7 +18,8 @@ const store: StoreOptions<IRootState> = {
     modules: {
         Drawer,
         User
-    }
+    },
+    plugins: [vuexLocal.plugin]
 };
 
 export default new Vuex.Store<IRootState>(store);
