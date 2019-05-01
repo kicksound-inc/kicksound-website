cf8ca3d9e48dc8bc708bdd670391d1b362ecfc4e
Tilkeii <imtilkeii@gmail.com>
2019-05-01 17:51:34 +0200

    add empty loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>LOADING</h1>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class Loading extends Vue {
+
+}
+</script>
