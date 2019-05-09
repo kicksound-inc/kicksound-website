39e0fa616bf01309a7170095d83d8518f61b8348
Tilkeii <imtilkeii@gmail.com>
2019-05-09 20:41:46 +0200

    add vee-validate

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import router from "./router";
 import store from "./store/store";
 import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
+import VeeValidate, { ValidationProvider } from "vee-validate";
 
 Vue.config.productionTip = false;
 
 Vue.use(Vuetify, {
     iconfont: "md",
 });
+
+Vue.use(VeeValidate, { inject: false });
 
 new Vue({
     router,
